4344367cc141636842ea07e156215dae91c4febc
Brayan Garcia <brayangapkb@gmail.com>
2021-04-28 19:15:33 -0500

    Avance escritorio hecho, faltan gastos maquetado

--- src/Components/Gastos/GastosLayout.js
+++ src/Components/Gastos/GastosLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+import {Querys} from '../utils/Querys';
 import FormGastos from './FormGastos';
 import ResumenGastos from './ResumenGastos';
 import Sidebar from './Sidebar';
@@ -9,22 +10,40 @@ import './Gastos.css';
 
 const GastosLayout = () => {
 
+    const queryLaptop = Querys('(min-width: 768px)');
+
+
     return (
-        <div className="layout-gastos py-2">
-            <div className="container">
+        <div className="layout-gastos py-2 py-md-0">
+            <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-2 d-none d-md-block">
+                    <div className="col d-none d-md-block sidebar-col min-vh-100">
                         <Sidebar/>
                     </div>
-                    <div className="col-md-10">
+                    <div className="col-md-10 presupuesto-col">
                         <div>
                             <div className="container-md-fluid">
                                 <div className="cantidad-presupuesto p-3">
                                     <h5 className="mb-0">Restante: $5000</h5> 
                                 </div>
-                                <div className="app-resumen-gastos">
-                                    <FormGastos/>
-                                    <ResumenGastos/>
+                                <div className="app-resumen-gastos row ">
+                                    <div className="mt-2 col-md-4">
+                                        <button type="button" className="btn btn-block btn-gastos-principal d-md-none" data-toggle="modal" data-target="#exampleModal">
+                                            Ingresar
+                                        </button>
+                                        <div className={queryLaptop ? 'd-block' : 'modal fade'} id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div className="modal-dialog modal-dialog-centered">
+                                                <div className="modal-content modal-form-gastos">
+                                                    <div className="modal-body">
+                                                        <FormGastos/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-8">
+                                        <ResumenGastos/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
